e1e7eebcae4683d688fea4f7cbad4d6275f574a5
bhuvanathamu <bhuvanakutty02@gmail.com>
2025-07-31 14:57:42 +0530

    program9

---
+++ program9.py
@@ -0,0 +1,10 @@
+print("Print Sum Of N numbers")
+print("-----------------------")
+n=int(input("Enter The n value:"))
+print("Result")
+s=n*n+1/2
+print("Sum:",s)
+ss=n*(n+1)*2*(n+1)/6
+print("Sum square:",ss)
+sc=n*(n+1)/2*n*(n+1)/2
+print("Sum cube:",sc)

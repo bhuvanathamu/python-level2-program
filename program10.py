324b94142b238f1aa6b22861eb538a5996aa1d03
bhuvanathamu <bhuvanakutty02@gmail.com>
2025-07-31 15:09:44 +0530

    program10

---
+++ program10.py
@@ -0,0 +1,14 @@
+print("Exponenential program")
+print("---------------------")
+x=int(input("Enter the x value:"))
+y=int(input("Enter the y value:"))
+z=int(input("Enter the z value:"))
+print("Result")
+p=x*x*x+y*y+z*z
+print("p:",p)
+q=x*x*x+y*y+z+20
+print("q:",q)
+r=x*x*y+y*y*z+z*z*x+10
+print("r:",r)
+s=x*x*x*y*y*z+2*x*y*z+x*y*z
+print("s:",s)
